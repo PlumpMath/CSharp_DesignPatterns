75bcd228cadc163b2cbe0572dc5ac916d4b91d76
ashafir <aviv@optimalfinance.co.il>
2014-09-25 20:09:48 +0300

    init

---
+++ TestCode/SingletonClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DesignStrategies_JonSkeet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCode
+{
+    [TestClass]
+    public class SingletonClient
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            NotASingleton s1 = new NotASingleton();
+            s1.DoSomething();
+            //what we dont want!
+            NotASingleton s2 = new NotASingleton();
+            s1.DoSomething();
+
+            //using a static class
+            SingletonLike.DoSomething();
+
+
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+
+            Assert.AreSame(singleton1, singleton2);
+        }
+    }
+}
